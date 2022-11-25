d43ff605f08764e4e11bd05a4ed50c0e9eae5f05
Faizhar Riskisyah <mohamad.faizhar@ruangguru.com>
2022-11-25 19:28:15 +0700

    Initial Commit

---
+++ tugas-2/tugas_2App.swift
@@ -0,0 +1,17 @@
+//
+//  tugas_2App.swift
+//  tugas-2
+//
+//  Created by Faizhar Riskisyah on 25/11/22.
+//
+
+import SwiftUI
+
+@main
+struct tugas_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
